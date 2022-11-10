20ac7da42fb22cdc319ddce143391093a5f40600
retadha <retadha.rumi@gmail.com>
2022-11-10 12:28:03 +0700

    Menambah proyek spring boot dan flutter

---
+++ flutter_rumahsehat/android/app/src/main/kotlin/apap/proyek/flutter_rumahsehat/MainActivity.kt
@@ -0,0 +1,6 @@
+package apap.proyek.flutter_rumahsehat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ rumahsehat/rumahsehat/src/main/java/apap/proyek/rumahsehat/RumahsehatApplication.java
@@ -0,0 +1,13 @@
+package apap.proyek.rumahsehat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RumahsehatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RumahsehatApplication.class, args);
+	}
+
+}
