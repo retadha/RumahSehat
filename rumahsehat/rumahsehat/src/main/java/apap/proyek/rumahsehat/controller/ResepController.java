557416ca0f874a554226a7214493b80fa1ccda46
charlespramudana <charles.pramudana@ui.ac.id>
2022-11-15 18:43:14 +0700

    menambahkan kondisi konfirmasi

--- rumahsehat/rumahsehat/src/main/java/apap/proyek/rumahsehat/controller/ResepController.java
+++ rumahsehat/rumahsehat/src/main/java/apap/proyek/rumahsehat/controller/ResepController.java
@@ -6,10 +6,13 @@ import apap.proyek.rumahsehat.service.JumlahService;
 import apap.proyek.rumahsehat.service.ResepService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -24,16 +27,31 @@ public class ResepController {
     private JumlahService jumlahService;
 
     @GetMapping("/resep/{idResep}")
-    public String viewDetailResep(@PathVariable long idResep, Model model){
+    public String viewDetailResep(@PathVariable long idResep, Model model, Authentication authentication){
+        String role = "";
+        if( authentication.getAuthorities().contains(new SimpleGrantedAuthority("Apoteker"))){
+            role="Apoteker";
+        }
         Resep resep = resepService.getResepById(idResep);
         List<Jumlah> listJumlah = jumlahService.findByResep(idResep);
         model.addAttribute("resep", resep);
         model.addAttribute("listJumlah", listJumlah);
+        model.addAttribute("role", role);
         return "resep/detail-resep";
-
     }
 
-
-
+    @PostMapping("/resep/{idResep}")
+    public String konfirmasiResep(@PathVariable long idResep, Model model, Authentication authentication){
+        String role = "";
+        if( authentication.getAuthorities().contains(new SimpleGrantedAuthority("Apoteker"))){
+            role="Apoteker";
+        }
+        Resep resep = resepService.getResepById(idResep);
+        List<Jumlah> listJumlah = jumlahService.findByResep(idResep);
+        model.addAttribute("resep", resep);
+        model.addAttribute("listJumlah", listJumlah);
+        model.addAttribute("role", role);
+        return "resep/detail-resep";
+    }
 
 }
