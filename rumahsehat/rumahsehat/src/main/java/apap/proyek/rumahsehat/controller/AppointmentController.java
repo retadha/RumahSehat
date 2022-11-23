46ae680e869ed2b1ec3f35c9e924835375ae58da
Yoseline Chandra <yoselinechan7@gmail.com>
2022-11-23 16:49:45 +0700

    done web jadwal appointment

--- rumahsehat/rumahsehat/src/main/java/apap/proyek/rumahsehat/controller/AppointmentController.java
+++ rumahsehat/rumahsehat/src/main/java/apap/proyek/rumahsehat/controller/AppointmentController.java
@@ -1,6 +1,7 @@
 package apap.proyek.rumahsehat.controller;
 
 import apap.proyek.rumahsehat.model.Appointment;
+import apap.proyek.rumahsehat.model.UserModel;
 import apap.proyek.rumahsehat.service.AppointmentService;
 import apap.proyek.rumahsehat.service.ResepService;
 import apap.proyek.rumahsehat.service.UserService;
@@ -12,7 +13,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,7 +36,7 @@ public class AppointmentController {
 
     //melihat semua jadwal appointment
     @GetMapping("/appointment")
-    public String listAppointment(Model model, Authentication authentication) {
+    public String listAppointment(Model model, Authentication authentication, Principal principal) {
         String role = "";
         if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("Admin"))) {
             role = "Admin";
@@ -40,11 +44,11 @@ public class AppointmentController {
         else if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("Dokter"))) {
             role = "Dokter";
         }
-        String currentUsernameId = authentication.getName();
+        UserModel user = userService.getUserByUsername(principal.getName());
         List<Appointment> listAppointment = appointmentService.getListAppointment();
         List<Appointment> listAppointmentDokter = new ArrayList<>();
         for (Appointment i : listAppointment) {
-            if (i.getDokter().getUser().getUuid() == currentUsernameId) {
+            if (i.getDokter().getUser().getUsername().equals(user.getUsername())) {
                 listAppointmentDokter.add(i);
             }
         }
@@ -57,8 +61,44 @@ public class AppointmentController {
     //melihat detail appointment
     @GetMapping("/appointment/{id}")
     public String viewDetailAppointment(@PathVariable String id, Model model, Authentication authentication) {
+        String role = "";
+        if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("Dokter"))) {
+            role = "Dokter";
+        }
+
         Appointment appointment = appointmentService.getAppointmentById(id);
+
+        boolean resepAda = false;
+        if (appointment.getResep() == null) {
+            resepAda = true;
+        }
+
         model.addAttribute("appointment", appointment);
+        model.addAttribute("role", role);
         return "appointment/view-appointment";
+    }
+
+    @PostMapping("/appointment/{id}")
+    public String selesaikanAppointment(@PathVariable String id, Model model, Authentication authentication, RedirectAttributes redirectAttributes) {
+        String role = "";
+        if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("Dokter"))) {
+            role = "Dokter";
+        }
+
+        Appointment appointment = appointmentService.getAppointmentById(id);
+        //jika resep tidak ada
+        if (appointment.getResep() == null) {
+            return "";
+        }
+        else {
+            //jika status resep sudah selesai
+            if (appointment.getResep().getIsDone() == true) {
+
+            }
+            //jika status resep belum selesai
+            redirectAttributes.addFlashAttribute("gagal", String.format("Resep masih belum selesai sehingga tidak appointment tidak dapat diselesaikan"));
+        }
+
+        return "redirect:/appointment/{id}";
     }
 }
