db964aa13cfc7703f5cee2f21bc0f8ac4e38b644
retadha <retadha.rumi@gmail.com>
2022-11-24 22:27:45 +0700

    fix conflict

--- rumahsehat/rumahsehat/src/main/java/apap/proyek/rumahsehat/repository/AppointmentDb.java
+++ rumahsehat/rumahsehat/src/main/java/apap/proyek/rumahsehat/repository/AppointmentDb.java
@@ -6,9 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface AppointmentDb extends JpaRepository<Appointment, String> {
-<<<<<<< HEAD
-    Optional<Appointment> findAppointmentById(String id);
-=======
+
     Optional<Appointment> findById(String id);
->>>>>>> 0f9f4b776734f27ffa01e7668dd305213a4bcf97
+
 }
