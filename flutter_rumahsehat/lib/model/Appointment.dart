19338f79256d76af3bbdc5aa04eb8cef8af0dff1
yoseline.chandra <yoseline.chandra@ui.ac.id>
2022-12-07 11:42:00 +0700

    test

--- flutter_rumahsehat/lib/model/Appointment.dart
+++ flutter_rumahsehat/lib/model/Appointment.dart
@@ -1,1 +1,8 @@
 import 'dart:convert';
+
+Appointment appointmentFromJson(String str) => Appointment.fromJson(json.decode(str));
+String appointmentToJson(Appointment data) => json.encode(data.toJson());
+
+class Appointment {
+
+}
---
+++ flutter_rumahsehat/lib/page/detail_appointment.dart
@@ -0,0 +1,5 @@
+import '../model/Appointment.dart';
+import 'package:http/http.dart' as http;
+import 'dart:core';
+import 'dart:convert';
+import 'package:flutter/material.dart';
