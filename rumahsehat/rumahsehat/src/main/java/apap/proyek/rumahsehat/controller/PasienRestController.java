e5f1083e9207b7b2014964ce79f619019abc15c3
Riris Melissa <rirismelissawinonatogatorop@Ririss-MacBook-Pro-2.local>
2022-12-07 12:25:25 +0700

    membuat fitur lihat profil pasien pada flutter

--- rumahsehat/rumahsehat/src/main/java/apap/proyek/rumahsehat/controller/PasienRestController.java
+++ rumahsehat/rumahsehat/src/main/java/apap/proyek/rumahsehat/controller/PasienRestController.java
@@ -1,48 +1,56 @@
 package apap.proyek.rumahsehat.controller;
 
 import apap.proyek.rumahsehat.model.Pasien;
+import apap.proyek.rumahsehat.model.UserModel;
 import apap.proyek.rumahsehat.service.PasienService;
+import apap.proyek.rumahsehat.service.UserService;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import java.util.Base64;
 import java.util.Map;
-
-@RestController
-@RequestMapping("/api")
-=======
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("/pasien")
->>>>>>> 20780cd (membuat fitur lihat profil pasien pada flutter)
+@RequestMapping("/api")
 public class PasienRestController {
     @Autowired
     private PasienService pasienService;
 
-<<<<<<< HEAD
+    @Autowired
+    private UserService userService;
+
     @GetMapping(value = "/pasien-tagihan/{kode}")
     private Map getPasienByTagihan(@PathVariable("kode") String kode){
         return pasienService.getPasienByTagihan(kode);
     }
 
-}
-=======
-    @GetMapping(value = "/api/viewall")
-    private List<Pasien> getListPasien() {
-        List<Pasien> listPasien = pasienService.findAll();
-        return listPasien;
+    @GetMapping(value = "/user" )
+    private UserModel getUserById(@RequestHeader("Authorization") String token) {
+        Map<String, String> decodedToken = decode(token);
+        String uuid = decodedToken.get("uuid");
+        UserModel user = userService.getUserById(uuid);
+        return user;
     }
 
-    @GetMapping(value = "/api/{idPasien}")
-    private Pasien getPasienById(String idPasien) {
-        Pasien pasien = pasienService.getPasienById(idPasien);
+    @GetMapping(value = "/pasien" )
+    private Pasien getPasienById(@RequestHeader("Authorization") String token) {
+        Map<String, String> decodedToken = decode(token);
+        String uuid = decodedToken.get("uuid");
+        Pasien pasien = pasienService.getPasienById(uuid);
         return pasien;
     }
+
+    private Map<String, String> decode(String token) {
+        String[] chunks = token.split("\\.");
+        Base64.Decoder decoder = Base64.getUrlDecoder();
+        String payload = new String(decoder.decode(chunks[1]));
+        Gson gson = new Gson();
+        Map<String, String> decodedToken = gson.fromJson(payload, new TypeToken<Map<String, String>>() {}.getType());
+        return decodedToken;
+    }
+
 }
->>>>>>> 20780cd (membuat fitur lihat profil pasien pada flutter)
