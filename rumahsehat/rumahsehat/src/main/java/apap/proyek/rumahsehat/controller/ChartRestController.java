d8220465c8fbd2cc9adf54a19ea2313caac6ba99
zidan <zidan.amukti@ui.ac.id>
2022-12-12 14:08:23 +0700

    action logging sm clean code

--- rumahsehat/rumahsehat/src/main/java/apap/proyek/rumahsehat/controller/ChartRestController.java
+++ rumahsehat/rumahsehat/src/main/java/apap/proyek/rumahsehat/controller/ChartRestController.java
@@ -4,26 +4,34 @@ import apap.proyek.rumahsehat.dto.chart.PendapatanBulanan;
 import apap.proyek.rumahsehat.dto.chart.PendapatanKumulatif;
 import apap.proyek.rumahsehat.dto.chart.PendapatanTahunan;
 import apap.proyek.rumahsehat.service.ChartService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Slf4j
 @RestController
-@RequestMapping("/chart")
+@RequestMapping("/api")
 public class ChartRestController {
     @Autowired
     private ChartService chartService;
 
-    @GetMapping(value = "/api/all")
-    private List<PendapatanTahunan> gYearlySalaries() {
-        return chartService.getThisYearSalary();
+    @RequestMapping(value = "/chart/all", method = RequestMethod.GET)
+    private ResponseEntity getYearlySalaries() {
+        log.info("api get all book");
+        ResponseEntity responseEntity = null;
+        try {
+            responseEntity = ResponseEntity.ok(chartService.getThisYearSalary());
+        } catch (Exception e) {
+            log.error("Error in get all salaries");
+            responseEntity = ResponseEntity.badRequest().body(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return responseEntity;
     }
 
-    @GetMapping("/api/line-chart-bulanan")
+    @GetMapping("/chart/line-chart-bulanan")
     private List<PendapatanBulanan> getMonthlySalary(
             @RequestParam(value = "id") List<String> values,
             @RequestParam(value = "bulan") String month,
@@ -31,14 +39,14 @@ public class ChartRestController {
         return chartService.getMonthlySalary(values, Integer.parseInt(month), Integer.parseInt(year));
     }
 
-    @GetMapping("/api/line-chart-tahunan")
+    @GetMapping("/chart/line-chart-tahunan")
     private List<PendapatanTahunan> getYearlySalary(
             @RequestParam(value = "id") List<String> values,
             @RequestParam(value = "tahun") String year) {
         return chartService.getYearlySalary(values, Integer.parseInt(year));
     }
 
-    @GetMapping("/api/kumulatif-chart")
+    @GetMapping("/chart/kumulatif-chart")
     private List<PendapatanKumulatif> getCumulativeSalary(
             @RequestParam(value = "id") List<String> values) {
         return chartService.getCumulativeSalary(values);
